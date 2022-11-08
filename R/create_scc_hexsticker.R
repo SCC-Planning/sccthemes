ba313c1622463a59e0db1c54445cfe046cd5ece6
Thomas Zwagerman <36264706+thomaszwagerman@users.noreply.github.com>
2022-11-08 09:17:10 +0000

    Add hexsticker function

---
+++ R/create_scc_hexsticker.R
@@ -0,0 +1,20 @@
+create_scc_sticker <- function(imgurl) {
+
+  get_scc_font()
+
+  hexSticker::sticker(
+    imgurl,
+    package = "sccthemes",
+    p_size = 15,
+    p_fontface = "bold",
+    s_x = 1,
+    s_y = 1.1,
+    s_width = 0.75,
+    s_height = 1,
+    p_x = 1.15,
+    p_y = 0.77,
+    h_fill = "#2d6ca2",
+    h_color = "#e8850c",
+    p_family = "ssp_font", filename="inst/figures/scc-hex.png"
+  )
+}
